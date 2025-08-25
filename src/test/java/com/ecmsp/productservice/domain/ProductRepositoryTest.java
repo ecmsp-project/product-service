bb106a3f27911d9f9d4f737fbdd5cbc187f167cf
Filip Pawłowicz <117346592+pawlowiczf@users.noreply.github.com>
2025-08-25 17:50:50 +0200

    create workflows to validate, build and push service to remote GCP registry (#8)
    * configure workflows for testing and deploying to GCP AR
    
    * create workflows to test and build service and push it to remote GCP registry
    
    * add Maven repository as primary one

--- src/test/java/com/ecmsp/productservice/domain/ProductRepositoryTest.java
+++ src/test/java/com/ecmsp/productservice/domain/ProductRepositoryTest.java
@@ -14,56 +14,56 @@ import java.util.Optional;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@Disabled
-@DataJpaTest
-@Import(TestEntitiesGenerator.class)
-public class ProductRepositoryTest {
-
-    @Autowired
-    private ProductRepository productRepository;
-
-    @Test
-    void testSaveAndFindById() {
-        // given
-        Product product = Product
-                .builder()
-                .name(TestDataGenerator.randomName())
-                .approximatePrice(TestDataGenerator.randomPrice())
-                .deliveryPrice(TestDataGenerator.randomPrice())
-                .description(TestDataGenerator.randomDescription())
-                .category(TestEntitiesGenerator.randomCategory())
-                .build();
-
-
-        // when
-        productRepository.save(product);
-        Optional<Product> found = productRepository.findById(product.getId());
-
-        // then
-        assertThat(found).isPresent();
-
-        assertThat(
-                found.map(Product::getId).orElseThrow()
-        ).isEqualTo(product.getId());
-
-        assertThat(
-                found.map(Product::getName).orElse("")
-        ).isEqualTo(product.getName());
-
-        Category category = found.map(Product::getCategory).orElse(null);
-        assertThat(category).isNotNull();
-        System.out.println(category);
-    }
-
-    @Test
-    void testSaveIncompleteProduct() {
-        // given
-        Product product = Product
-                .builder()
-                .category(null)
-                .name("")
-                .build();
-
-        assertThatThrownBy(() -> productRepository.saveAndFlush(product));
-    }
-}
+//@DataJpaTest
+//@Disabled
+//@Import(TestEntitiesGenerator.class)
+//public class ProductRepositoryTest {
+//
+//    @Autowired
+//    private ProductRepository productRepository;
+//
+//    @Test
+//    void testSaveAndFindById() {
+//        // given
+//        Product product = Product
+//                .builder()
+//                .name(TestDataGenerator.randomName())
+//                .approximatePrice(TestDataGenerator.randomPrice())
+//                .deliveryPrice(TestDataGenerator.randomPrice())
+//                .description(TestDataGenerator.randomDescription())
+//                .category(TestEntitiesGenerator.randomCategory())
+//                .build();
+//
+//
+//        // when
+//        productRepository.save(product);
+//        Optional<Product> found = productRepository.findById(product.getId());
+//
+//        // then
+//        assertThat(found).isPresent();
+//
+//        assertThat(
+//                found.map(Product::getId).orElseThrow()
+//        ).isEqualTo(product.getId());
+//
+//        assertThat(
+//                found.map(Product::getName).orElse("")
+//        ).isEqualTo(product.getName());
+//
+//        Category category = found.map(Product::getCategory).orElse(null);
+//        assertThat(category).isNotNull();
+//        System.out.println(category);
+//    }
+//
+//    @Test
+//    void testSaveIncompleteProduct() {
+//        // given
+//        Product product = Product
+//                .builder()
+//                .category(null)
+//                .name("")
+//                .build();
+//
+//        assertThatThrownBy(() -> productRepository.saveAndFlush(product));
+//    }
+//}
