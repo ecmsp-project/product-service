80c981e2d46ebec011c955545253204571169181
Ariel Michalik <arie.michalik@gmail.com>
2025-12-01 18:56:30 +0100

    comment test

--- src/test/java/com/ecmsp/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/ecmsp/productservice/ProductServiceApplicationTests.java
@@ -3,11 +3,11 @@ package com.ecmsp.productservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ProductServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class ProductServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
